ef504136f4b5a24a8603e3b7fc96f7d315279931
张斌 <bin.zhang@appshahe.com>
2024-07-29 15:09:19 +0800

    feat：自动生成模块枚举工具

---
+++ Assets/ZMAssetsFrameWork/Config/BundleModuleEnum.cs
@@ -0,0 +1,16 @@
+/*
+ *-------------------------
+ *Title:AssetBundle模块类
+ *Author:ZHANGBIN
+ *Date:2024/7/29 15:00:47
+ *注意：以下文件是自动生成的，再次生成会覆盖原有的代码，若修改尽量不要自动生成
+ *--------------------------
+ */
+namespace ZMAssetFrameWork
+{
+	public enum BundleModuleEnum
+	{
+		None,
+		Game,
+	}
+}
---
+++ Assets/ZMAssetsFrameWork/Editor/BundleTools.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+public class BundleTools
+{
+    public static string _bundleModuleEnumFilePath = Application.dataPath + "/ZMAssetsFrameWork/Config/BundleModuleEnum.cs";
+
+    [MenuItem("ZMAssetFrameWork/Generate BundleModuleEnum")]
+    public static void GenerateBundleModuleEnum()
+    {
+        string namespacename = "ZMAssetFrameWork";
+        string eumname = "BundleModuleEnum";
+        
+        if(File.Exists(_bundleModuleEnumFilePath))
+        {
+            File.Delete(_bundleModuleEnumFilePath);
+            AssetDatabase.Refresh();
+        }
+        
+        StreamWriter writer = File.CreateText(_bundleModuleEnumFilePath);
+        writer.WriteLine("/*");
+        writer.WriteLine(" *-------------------------");
+        writer.WriteLine(" *Title:AssetBundle模块类");
+        writer.WriteLine(" *Author:ZHANGBIN");
+        writer.WriteLine(" *Date:" + DateTime.Now);
+        writer.WriteLine(" *注意：以下文件是自动生成的，再次生成会覆盖原有的代码，若修改尽量不要自动生成");
+        writer.WriteLine(" *--------------------------");
+        writer.WriteLine(" */");
+        
+        writer.WriteLine($"namespace {namespacename}");
+        writer.WriteLine("{");
+        
+        List<BundleModuleData> moduleDataList = BuildBundleConfigura.Instance.AssetBundleConfig;
+
+        if (moduleDataList == null || moduleDataList.Count <= 0)
+        {
+            return;
+        }
+        writer.WriteLine($"\tpublic enum {eumname}");
+        writer.WriteLine("\t{");
+        writer.WriteLine("\t\tNone,");
+
+        for (int i = 0; i < moduleDataList.Count; i++)
+        {
+            writer.WriteLine($"\t\t{moduleDataList[i].moduleName},");
+        }
+        writer.WriteLine("\t}");
+        writer.WriteLine("}");
+        writer.Flush();
+        writer.Close();
+        
+        AssetDatabase.Refresh();
+    }
+}
