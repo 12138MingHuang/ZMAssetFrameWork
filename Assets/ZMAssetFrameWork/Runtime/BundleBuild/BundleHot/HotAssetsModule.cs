c4905e91373331efcd43f1b2c002e93d425f9a5c
张斌 <bin.zhang@appshahe.com>
2024-08-02 18:58:30 +0800

    feat：计算热更文件大小

--- Assets/ZMAssetFrameWork/Runtime/BundleBuild/BundleHot/HotAssetsModule.cs
+++ Assets/ZMAssetFrameWork/Runtime/BundleBuild/BundleHot/HotAssetsModule.cs
@@ -14,6 +14,24 @@ namespace ZMAssetFrameWork
     public class HotAssetsModule
     {
         /// <summary>
+        /// 热更资源下载存储路径
+        /// </summary>
+        public string HotAssetsSavePath
+        {
+            get { return Application.persistentDataPath + "/HotAssets/" + CurBundleModuleEnum + "/"; }
+        }
+
+        /// <summary>
+        /// 所有热更的资源列表
+        /// </summary>
+        private List<HotFileInfo> _allDownLoadAssetsList = new List<HotFileInfo>();
+        
+        /// <summary>
+        /// 需要下载的资源列表
+        /// </summary>
+        private List<HotFileInfo> _needDownLoadAssetsList = new List<HotFileInfo>();
+
+        /// <summary>
         /// 服务端资源清单
         /// </summary>
         private HotAssetsManifest _serverHotAssetsManifest;
@@ -32,17 +50,25 @@ namespace ZMAssetFrameWork
         /// 本地资源热更清单存储路径
         /// </summary>
         private string _localHotAssetsManifestPath;
-        
+
         /// <summary>
         /// 当前下载的资源模块类型
         /// </summary>
         public BundleModuleEnum CurBundleModuleEnum { get; set; }
-        
+
         /// <summary>
         /// 下载所有资源完成的回调
         /// </summary>
         public Action<BundleModuleEnum> onDownLoadAllAssetsFinish;
+        
+        /// <summary>
+        /// 最大下载资源大小
+        /// </summary>
+        private float AssetsMaxSizeM { get; set; }
 
+        /// <summary>
+        /// Mono脚本
+        /// </summary>
         private MonoBehaviour _monoBehaviour;
 
         public HotAssetsModule(BundleModuleEnum bundleModuleEnum, MonoBehaviour monoBehaviour)
@@ -61,10 +87,7 @@ namespace ZMAssetFrameWork
         {
             this.onDownLoadAllAssetsFinish = hotFinish;
 
-            if (isCheckAssetsVersion)
-            {
-                
-            }
+            if (isCheckAssetsVersion) { }
         }
 
         /// <summary>
@@ -78,21 +101,56 @@ namespace ZMAssetFrameWork
             _monoBehaviour.StartCoroutine(DownLoadHotAssetsManifest(() =>
             {
                 //资源下载完成
-                
                 //1.检测当前版本是否需要热更
                 if (CheckModuleAssetsIsHot())
                 {
-                    
+                    HotAssetsPatch serverHotAssetsPatch = _serverHotAssetsManifest.hotAssetsPatchList[_serverHotAssetsManifest.hotAssetsPatchList.Count - 1];
+                    bool isNeedHot = ComputeNeedHotAssetsList(serverHotAssetsPatch);
+                    if (isNeedHot)
+                    {
+                        checkCallBack?.Invoke(true, AssetsMaxSizeM);
+                    }
+                    else
+                    {
+                        checkCallBack.Invoke(false, 0);
+                    }
                 }
                 else
                 {
-                    
+                    checkCallBack.Invoke(false, 0);
                 }
-                
+
                 //2.如果需要热更，开始计算需要下载的文件，开始下载文件
-                
+
                 //3.如果不需要热更，说明文件是新的，直接热更完成
             }));
+        }
+
+        /// <summary>
+        /// 计算需要热更的文件列表
+        /// </summary>
+        /// <param name="serverHotAssetsPatch">服务端热更补丁包</param>
+        /// <returns>是否需要热更</returns>
+        private bool ComputeNeedHotAssetsList(HotAssetsPatch serverHotAssetsPatch)
+        {
+            if (!Directory.Exists(HotAssetsSavePath))
+            {
+                Directory.CreateDirectory(HotAssetsSavePath);
+            }
+
+            foreach (HotFileInfo item in serverHotAssetsPatch.hotAssetsList)
+            {
+                //获取本地AssetBundle文件路径
+                string localFilePath = HotAssetsSavePath + item.abName;
+                _allDownLoadAssetsList.Add(item);
+                //如果本地文件不存在，或者本地文件与服务端不一致，就需要热更
+                if (!File.Exists(localFilePath) || item.md5 != MD5.GetMd5FromFile(localFilePath))
+                {
+                    _needDownLoadAssetsList.Add(item);
+                    AssetsMaxSizeM += item.size / 1024.0f;
+                }
+            }
+            return _needDownLoadAssetsList.Count > 0;
         }
 
         /// <summary>
